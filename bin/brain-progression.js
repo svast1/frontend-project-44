befe7fdfa8efe8d42d4c35e2cdf12ce5975b49a9
svast1 <dubboylbox@mail.ru>
2022-12-07 14:21:08 +0300

    brain-progression fix

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 /* eslint no-useless-concat: "off", curly: "error" */
-/* eslint no-inner-declarations: "off", curly: "error" */
 
 import readlineSync from 'readline-sync';
 
@@ -10,41 +9,31 @@ let flag = 'win';
 let win = 0;
 const name = readlineSync.question('May I have your name? ');
 console.log('Hello,' + ' ' + `${name}` + '!');
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+console.log('What number is missing in the progression?');
 while (flag === 'win') {
-  const num = Math.floor(Math.random() * (0 - 100) + 100);
-  console.log('Question: ' + `${num}`);
+  const length = Math.floor(Math.random() * (5 - 10) + 10);
+  const increase = Math.floor(Math.random() * (2 - 5) + 5);
+  let num = (Math.floor(Math.random() * (0 - 40) + 40));
+  const randomIndex = Math.floor(Math.random() * (0 - length) + length);
+  const array = [];
+  array.push(num);
+  while (array.length !== length) {
+    array.push(num += increase);
+  }
+  const check = String(array[randomIndex]);
+  array[randomIndex] = '..';
+  const Question = array.join(' ');
+  console.log('Question: ' + `${Question}`);
   const answer = readlineSync.question('Your answer: ');
-  if (answer !== 'yes' && answer !== 'no') {
-    console.log('"yes" is wrong answer ;(. Correct answer was "no".' + '\n' + "Let's try again," + ' ' + `${name}` + '!');  
-    flag = 'lose';
-  }
-  function prime(n) {
-    const m = Math.sqrt(n);
-    let isPrime = 'true';
-    for (let i = 2; i <= m; i += 1) {
-      if (n % i === 0) {
-        isPrime = 'false';
-        break;
-      }
-    }
-    return isPrime;
-  }
-  const check = prime(num);
-  if (check === 'true' && answer === 'yes') {
+  if (answer === check) {
     console.log('Correct!');
     win += 1;
-  } if (check !== 'true' && answer === 'no') {
-    console.log('Correct!');
-    win += 1;
-  } if (check === 'true' && answer === 'no') {
-    console.log('"no" is wrong answer ;(. Correct answer was "yes".' + '\n' + "Let's try again," + ' ' + `${name}` + '!');  
-    flag = 'lose';
-  } if (check !== 'true' && answer === 'yes') {
-    console.log('"yes" is wrong answer ;(. Correct answer was "no".' + '\n' + "Let's try again," + ' ' + `${name}` + '!');  
+  } if (answer !== check) {
+    console.log(`${answer}` + ' ' + 'is wrong answer ;(. Correct answer was' + ' ' + `${check}` + '.' + '\n' + "Let's try again," + ' ' + `${name}` + '!');
     flag = 'lose';
   } if (win === 3) {
     console.log('Congratulations,' + ' ' + `${name}` + '!');
     break;
   }
 }
+
