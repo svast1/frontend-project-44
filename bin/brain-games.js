d4ec9e07dfbaafc9c163f1d2fe3f4553139e199f
svast1 <dubboylbox@mail.ru>
2022-12-07 14:46:47 +0300

    brain-games fix #999

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /* eslint no-useless-concat: "off", curly: "error" */
 import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log('Hello,' + ' ' + `${name}` + '!');
-
