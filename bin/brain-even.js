5aecc0451081ce75ff237ecad51c090d15fb8668
svast1 <dubboylbox@mail.ru>
2022-12-07 14:11:47 +0300

    brain-even fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -16,7 +16,7 @@ console.log('Question:' + ' ' + `${Question}`);
 const answer = readlineSync.question('Your answer: ');
 if (answer !== 'yes' && answer !== 'no') {
    flag = 'lose'
-   console.log('"yes" is wrong answer ;(. Correct answer was "no".', '\n', 'Lets try again,' + ' ' + `${name}` + '!')
+   console.log('"yes" is wrong answer ;(. Correct answer was "no".', '\n', "Let's try again," + ' ' + `${name}` + '!')
 }
 if (Question % 2 === 0 && answer === 'yes') {
 console.log('Correct!')
@@ -25,15 +25,15 @@ win += 1
 console.log('Correct!')
 win += 1
 } if (Question % 2 !== 0 && answer === 'yes') {
-console.log('"yes" is wrong answer ;(. Correct answer was "no".', '\n', 'Lets try again,' + ' ' + `${name}` + '!')
+console.log('"yes" is wrong answer ;(. Correct answer was "no".', '\n', "Let's try again," + ' ' + `${name}` + '!')
 flag = 'lose'
 }
 if (Question % 2 === 0 && answer === 'no') {
-console.log('"no" is wrong answer ;(. Correct answer was "yes".', '\n', 'Lets try again,' + ' ' + `${name}` + '!')
+console.log('"no" is wrong answer ;(. Correct answer was "yes".', '\n', "Let's try again," + ' ' + `${name}` + '!')
 flag = 'lose'
 }
 if (win === 3) {
-  console.log('Congratilutions,' + ' ' + `${name}` + '!')
+  console.log('Congratulations,' + ' ' + `${name}` + '!')
   break
 }
 }
